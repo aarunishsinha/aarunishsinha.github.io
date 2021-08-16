1ff2d8cfb2423ae0f124e8d88c41d97ca1e2a088
Aarunish Sinha <58175652+aarunishsinha@users.noreply.github.com>
2021-08-16 21:02:35 +0530

    update header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -29,7 +29,7 @@ export default class Header extends Component {
               <h1>I'm Aarunish Sinha.</h1>
               <h3>
                 I'm a <span>Senior Undergraduate Student</span> majoring in <span>Computer Science and Engineering</span> at <span>Indian Institute of Technology, Delhi</span>. Let's <a className="smoothscroll" href="#about">start scrolling</a>
-                and learn more <a className="smoothscroll" href="#about">about me</a>.
+                 and learn more <a className="smoothscroll" href="#about">about me</a>.
               </h3>
               <div className="s-hero__content-social">
                 <a href="https://www.facebook.com/Aarunish"><i className="fab fa-facebook-square" aria-hidden="true" /></a>
@@ -41,6 +41,14 @@ export default class Header extends Component {
               </div>
             </div> {/* end s-hero__content-about */}
           </div>
+        </div>
+        <div className="s-hero__scroll">
+          <a href="#about" className="s-hero__scroll-link smoothscroll">
+            <span className="scroll-arrow">
+              <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" style={{fill: 'rgba(0, 0, 0, 1)', transform: '', msFilter: ''}}><path d="M18.707 12.707L17.293 11.293 13 15.586 13 6 11 6 11 15.586 6.707 11.293 5.293 12.707 12 19.414z" /></svg>
+            </span>
+            <span className="scroll-text">Scroll Down</span>
+          </a>
         </div>
       </section>
       </React.Fragment>
